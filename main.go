d4ea88b8578df9c14b23bf9f19ee340c50cc010a
Javi Moreno <javiermorenog2a@gmail.com>
2023-07-16 18:38:54 -0300

    Funciones

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"cursodegodesde0/variables"
+	"fmt"
 )
 
 func main() {
-	variables.ShowIntegers()
+	// variables.RestoVariables()
+	estado, texto := variables.ConviertoATexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
