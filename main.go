ffec6d0be0b06b417d86d1c80c7ca0c3faddac99
Javi Moreno <javiermorenog2a@gmail.com>
2023-07-16 19:14:19 -0300

    Ingresar datos por pantalla

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
-import (
-	"cursodegodesde0/ejercicios"
-	"fmt"
-)
+import "cursodegodesde0/teclado"
 
 func main() {
-	numero, mensaje := ejercicios.EsMayorA100("50")
-	fmt.Println(numero)
-	fmt.Println(mensaje)
+	// numero, mensaje := ejercicios.EsMayorA100("50")
+	// fmt.Println(numero)
+	// fmt.Println(mensaje)
+
+	teclado.IngresoNumeros()
 }
