4a850ede89713808fb3eef9fb52c108894d353d1
Javi Moreno <javiermorenog2a@gmail.com>
2023-07-16 19:26:59 -0300

    iteraciones

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-import "cursodegodesde0/teclado"
+import "cursodegodesde0/iteraciones"
 
 func main() {
 	// numero, mensaje := ejercicios.EsMayorA100("50")
 	// fmt.Println(numero)
 	// fmt.Println(mensaje)
 
-	teclado.IngresoNumeros()
+	// teclado.IngresoNumeros()
+	iteraciones.Iterar()
 }
