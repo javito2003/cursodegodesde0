1d618ca893c541f9a634f4da6e5daf19b49a8238
Javi Moreno <javiermorenog2a@gmail.com>
2023-07-16 18:58:22 -0300

    Condicionales

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
+	"cursodegodesde0/ejercicios"
 	"fmt"
-	"runtime"
 )
 
 func main() {
-	// variables.RestoVariables()
-	// estado, texto := variables.ConviertoATexto(1588)
-	// fmt.Println(estado)
-	// fmt.Println(texto)
-	
-	os := runtime.GOOS
-	// if os := runtime.GOOS; os == "Linux." 
-	if os == "linux" {
-		fmt.Println("Soy linux")
-	} else {
-		fmt.Println("Soy Windows")
-	}
-
-	switch os {
-	case "linux":
-		fmt.Println("Esto es linux")
-	case "darwin":
-		fmt.Println("Esto es darwin")
-	default:
-		fmt.Printf("%s \n", os)
-	}
+	numero, mensaje := ejercicios.EsMayorA100("50")
+	fmt.Println(numero)
+	fmt.Println(mensaje)
 }
